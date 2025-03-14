38dfc2821d0a531bc3e9a32d43e01c1068aae834
Abba002 <abbanaryu@gmail.com>
2025-03-14 19:23:13 -0400

    fixed bugs

--- counter.srcs/constrs_1/imports/counter/Nexys-A7-100T-Master.xdc
+++ counter.srcs/constrs_1/imports/counter/Nexys-A7-100T-Master.xdc
@@ -64,7 +64,7 @@ set_property -dict { PACKAGE_PIN K13   IOSTANDARD LVCMOS33 } [get_ports { seg[3]
 set_property -dict { PACKAGE_PIN P15   IOSTANDARD LVCMOS33 } [get_ports { seg[4] }]; #IO_L13P_T2_MRCC_14 Sch=ce
 set_property -dict { PACKAGE_PIN T11   IOSTANDARD LVCMOS33 } [get_ports { seg[5] }]; #IO_L19P_T3_A10_D26_14 Sch=cf
 set_property -dict { PACKAGE_PIN L18   IOSTANDARD LVCMOS33 } [get_ports { seg[6] }]; #IO_L4P_T0_D04_14 Sch=cg
-#set_property IOSTANDARD LVCMOS33 [get_ports seg];
+
 #set_property -dict { PACKAGE_PIN H15   IOSTANDARD LVCMOS33 } [get_ports { DP }]; #IO_L19N_T3_A21_VREF_15 Sch=dp
 
 set_property -dict { PACKAGE_PIN J17   IOSTANDARD LVCMOS33 } [get_ports { an[0] }]; #IO_L23P_T3_FOE_B_15 Sch=an[0]
@@ -75,7 +75,7 @@ set_property -dict { PACKAGE_PIN J14   IOSTANDARD LVCMOS33 } [get_ports { an[3] 
 #set_property -dict { PACKAGE_PIN T14   IOSTANDARD LVCMOS33 } [get_ports { AN[5] }]; #IO_L14P_T2_SRCC_14 Sch=an[5]
 #set_property -dict { PACKAGE_PIN K2    IOSTANDARD LVCMOS33 } [get_ports { AN[6] }]; #IO_L23P_T3_35 Sch=an[6]
 #set_property -dict { PACKAGE_PIN U13   IOSTANDARD LVCMOS33 } [get_ports { AN[7] }]; #IO_L23N_T3_A02_D18_14 Sch=an[7]
-#set_property IOSTANDARD LVCMOS33 [get_ports an];
+
 
 ##Buttons
 
--- counter.v
+++ counter.v
@@ -8,12 +8,12 @@ reg button_state =0;
 
 always @(posedge clk) begin
     if(button == button_state)
-        count = 0;
+        count<= 0;
     else begin
-        count = count+1;
+        count <= count+1;
         if (count==1000000) begin 
-            button_state = button;
-            clean_button = button;
+            button_state <= button;
+            clean_button <= button;
         end
     end
 end
@@ -40,21 +40,21 @@ endmodule
 
 module seven_seg_decoder(
     input [3:0] digit,
-    output reg [6:0] seg 
+    output reg [6:0] segment 
 );
 always @(*) begin
     case(digit)
-        4'b0000: seg = 7'b1000000; //0
-        4'b0001: seg = 7'b1111001;//1
-        4'b0010: seg = 7'b0100100;//2
-        4'b0011: seg = 7'b0011001;//3
-        4'b0100: seg = 7'b0011001;//4
-        4'b0101: seg = 7'b0010010;//5
-        4'b0110: seg = 7'b0000010;//6
-        4'b0111: seg = 7'b1111000;//7
-        4'b1000: seg = 7'b0000000;//8
-        4'b1001: seg = 7'b0010000;//9
-        default: seg = 7'b1111111;//off
+        4'b0000: segment = 7'b1000000; //0
+        4'b0001: segment = 7'b1111001;//1
+        4'b0010: segment = 7'b0100100;//2
+        4'b0011: segment = 7'b0110000;//3
+        4'b0100: segment = 7'b0011001;//4
+        4'b0101: segment = 7'b0010010;//5
+        4'b0110: segment = 7'b0000010;//6
+        4'b0111: segment = 7'b1111000;//7
+        4'b1000: segment = 7'b0000000;//8
+        4'b1001: segment = 7'b0010000;//9
+        default: segment = 7'b1111111;//off
     endcase
 end
 endmodule
@@ -78,7 +78,7 @@ debouncer debounce_rst(.clk(clk), .button(btn_rst), .clean_button(clean_rst));
 counter count_mod(.clk(clk), .inc_button(clean_inc), .reset_button(clean_rst), .count(count));
 
 //seven segment decoder
-seven_seg_decoder seg_dec(.digit(count), .seg(segs));
+seven_seg_decoder seg_dec(.digit(count), .seg(seg));
 
 //enable one display
 assign an = 4'b1110; 
